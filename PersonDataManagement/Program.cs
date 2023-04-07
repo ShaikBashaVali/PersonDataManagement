f36bacd7611390feac546de66301dc82bcd04972
Shaik Basha Vali <bashavali.shaik@outlook.com>
2023-04-08 03:15:49 +0530

    [Basha] Skip Record Lessthan 60

--- PersonDataManagement/Program.cs
+++ PersonDataManagement/Program.cs
@@ -13,6 +13,7 @@
             Console.WriteLine("Enter 3 Retrieve Records which is Age between 13 to 18");
             Console.WriteLine("Enter 4. Calculating average age");
             Console.WriteLine("Enter 5.Search Specific name in list");
+            Console.WriteLine("Enter 6. Skip record which less than 60 ");
             Console.WriteLine("Enter the option");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -34,6 +35,9 @@
                     break;
                 case 5:
                     PersonData.SearchingSpecificName(list, "Arun");
+                    break;
+                case 6:
+                    PersonData.SkipRecordLessThan60(list);
                     break;
                 default:
                     Console.WriteLine("Enter valid option");
